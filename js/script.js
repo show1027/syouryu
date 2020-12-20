af12447d1e7d27d6b42e2256b7a3b01f83c5e43c
Shota Hattori <67850578+show1027@users.noreply.github.com>
2020-12-20 17:48:00 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+(function($) {
+    var $nav   = $('#navArea');
+    var $btn   = $('.toggle_btn');
+    var $mask  = $('#mask');
+    var open   = 'open'; // class
+    // menu open close
+    $btn.on( 'click', function() {
+      if ( ! $nav.hasClass( open ) ) {
+        $nav.addClass( open );
+      } else {
+        $nav.removeClass( open );
+      }
+    });
+    // mask close
+    $mask.on('click', function() {
+      $nav.removeClass( open );
+    });
+} )(jQuery);
+
+$(document).ready(function() {
+    var pagetop = $('#pagetop');
+      $(window).scroll(function () {
+         if ($(this).scrollTop() > 100) {
+              pagetop.fadeIn();
+         } else {
+              pagetop.fadeOut();
+              }
+         });
+         pagetop.click(function () {
+             $('body, html').animate({ scrollTop: 0 }, 500);
+                return false;
+     });
+  });  
